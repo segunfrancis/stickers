fb762d6e787d8d714acff3b1b3f8eaf9d39f7f13
Simon Xu <simonxuzhe@whatsapp.com>
2019-05-25 08:18:29 -0700

    add ability to set any number of packs in the app

--- Android/app/src/main/java/com/example/samplestickerapp/EntryActivity.java
+++ Android/app/src/main/java/com/example/samplestickerapp/EntryActivity.java
@@ -9,14 +9,18 @@
 package com.example.samplestickerapp;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.util.Pair;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
@@ -38,8 +42,49 @@ public class EntryActivity extends BaseActivity {
         loadListAsyncTask.execute();
     }
 
+    private void showNumberOfPacksChooser(final StickerPack stickerPack) {
+        final EditText txtUrl = new EditText(this);
+
+        txtUrl.setHint("A number from 1 to 1000");
+
+        new AlertDialog.Builder(this)
+                .setTitle("Select number of sticker packs")
+                .setMessage("How many packs do you want this app to have?")
+                .setView(txtUrl)
+                .setPositiveButton("Set", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String numberString = txtUrl.getText().toString();
+                        try {
+                            int selectedNumber = Integer.parseInt(numberString);
+                            if (selectedNumber <= 0) {
+                                throw new IllegalArgumentException("The number needs to be bigger than 0");
+                            }
+                            handleNumberOfPacks(stickerPack, selectedNumber);
+
+                        } catch (NumberFormatException ex) {
+                            Toast.makeText(EntryActivity.this, "Please put in only numbers. Press back and launch the app again", Toast.LENGTH_LONG).show();
+                        } catch (IllegalArgumentException ex) {
+                            Toast.makeText(EntryActivity.this, "The number needs to be bigger than 0. Press back and launch the app again", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                })
+                .show();
+    }
+
     private void showStickerPack(ArrayList<StickerPack> stickerPackList) {
         progressBar.setVisibility(View.GONE);
+        showNumberOfPacksChooser(stickerPackList.get(0));
+    }
+
+    void handleNumberOfPacks(StickerPack singlePack, int selectedNumber) {
+        ArrayList<StickerPack> stickerPackList = new ArrayList<>(selectedNumber);
+        for (int i = 0; i < selectedNumber; i++) {
+            stickerPackList.add(singlePack);
+        }
         if (stickerPackList.size() > 1) {
             final Intent intent = new Intent(this, StickerPackListActivity.class);
             intent.putParcelableArrayListExtra(StickerPackListActivity.EXTRA_STICKER_PACK_LIST_DATA, stickerPackList);
